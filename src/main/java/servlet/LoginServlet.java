d0ddd6c63a307083cdb6b732f9a76168049dff99
cocco <yoshihukuoka@gamil.com>
2022-09-14 18:57:18 +0900

    a

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -18,7 +18,7 @@ public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
         dispatcher.forward(request, response);
     }
 
